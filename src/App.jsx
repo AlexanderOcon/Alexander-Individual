b822571833210256f293bf343cc63db30445eb20
AlexanderOcon <brandon.hermida.ocon@gmail.com>
2026-03-26 15:22:02 -0600

    Cambios del E5 atrasado

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,39 @@
 import { useState } from "react";
+import { BrowserRouter as Router,Routes, Route } from "react-router-dom";
+
+import Encabezado from "./assets/components/navegacion/Encabezado";
+import RutaProtegida from "./assets/components/rutas/RutaProtegida";
+import Inicio from "./assets/views/Inicio";
+import Categorias from "./assets/views/Categorias";
+import Catalogo from "./assets/views/Catalogo";
+import Productos from "./assets/views/Productos";
+import Login from "./assets/views/Login";
+import Pagina404 from "./assets/views/Pagina404";
 
 import "./App.css";
+
+
 const App = () => {
+
   return (
-    <>
-      <h1>Componente Principal</h1>
-    </>
+    <Router>
+      <Encabezado />
+      
+      <main className="margen-superior-main">
+        <Routes>
+
+          <Route path="/login" element={<Login />} />
+
+          <Route path="/" element={<RutaProtegida><Inicio/>/</RutaProtegida>} />
+          <Route path="/categorias" element={<RutaProtegida><Categorias/>/</RutaProtegida>} />
+          <Route path="/catalogo" element={<RutaProtegida><Catalogo/>/</RutaProtegida>} />
+          <Route path="/productos" element={<RutaProtegida><Productos/>/</RutaProtegida>} />
+
+          <Route path="*" element={<Pagina404 />} />
+
+        </Routes>
+      </main>
+    </Router>
   );
 }
 
