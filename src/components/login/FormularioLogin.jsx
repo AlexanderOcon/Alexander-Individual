755877281bc37889fbf8ab1b6520cd91d66ba816
AlexanderOcon <brandon.hermida.ocon@gmail.com>
2026-04-08 08:48:18 -0600

    Cambios bananos

--- src/components/login/FormularioLogin.jsx
+++ src/components/login/FormularioLogin.jsx
@@ -1,46 +1,43 @@
 import React from "react";
-import {Form, Button, Card, Alert}from "react-bootstrap";
+import { Form, Button, Card, Alert } from "react-bootstrap";
 
-const FormularioLogin = ({usuario,contrasena, error, setUsuario, setContrasena, IniciarSesion}) => {
-
+const FormularioLogin = ({usuario,contrasena,error,setUsuario,setContrasena,IniciarSesion,}) => {
     return (
-        <Card style={{ minWidth:"32@px", maxWidth:"4@@px", width:"100%" }} className="p-4 shadow-1g">
-            <Card.Body>
-                <h3 className="text-center mb-4">Iniciar Sesión </h3>
-
-    {error && <Alert variant="danger">{error}</Alert>}
-
-    <Form>
+    <Card
+    style={{ minWidth: "32@px", maxWidth: "4@@px", width: "100%" }}
+    className="p-4 shadow-1g"
+    >
+    <Card.Body>
+        <h3 className="text-center mb-4">Iniciar Sesión </h3>
+        {error && <Alert variant="danger">{error}</Alert>}
+        <Form>
         <Form.Group className="mb--3" controlId="usuario">
             <Form.Label>Usuario</Form.Label>
             <Form.Control
             type="text"
             placeholder="Ingresa tu usuario"
             value={usuario}
-            onChange={(e)=> setUsuario(e.target.value)}
+            onChange={(e) => setUsuario(e.target.value)}
             required
-        />
+            />
         </Form.Group>
-
         <Form.Group className="mb-3" controlId="contrasena">
             <Form.Label>Contraseña</Form.Label>
             <Form.Control
-                type="password"
-                placeholder="Ingresa tu contraseña"
-                value={contrasena}
-                onChange={(e)=> setContrasena(e.target.value)}
-                required
+            type="password"
+            placeholder="Ingresa tu contraseña"
+            value={contrasena}
+            onChange={(e) => setContrasena(e.target.value)}
+            required
             />
         </Form.Group>
-
         <Button variant="primary" className="w-100" onClick={IniciarSesion}>
             Iniciar Sesión
         </Button>
-    </Form>
-</Card.Body>
-</Card>
-
-    );
+        </Form>
+    </Card.Body>
+    </Card>
+);
 };
 
 const estiloContenedor = {
@@ -57,5 +54,4 @@ const estiloContenedor = {
   padding: "20px",
 };
 
-
-export default FormularioLogin; 
+export default FormularioLogin;
--- vite.config.js
+++ vite.config.js
@@ -6,10 +6,5 @@ export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
     include: ["tslib"]
-  },
-  build: {
-    rollupOptions: {
-      external: ["tslib"]
-    }
   }
 })
